86089bacb67a67a4a9f634ffbd6be7b5a7feddd7
Giuseppe Romagnuolo <Giuseppe.Romagnuolo@allenovery.com>
2019-04-30 23:56:03 +0100

    Refactor: tidied up code

--- ModuleScaffolder.py
+++ ModuleScaffolder.py
@@ -1,14 +1,30 @@
+"""
+Module responsible for collating all source files into a single file.
+This is a utility in order to upload the source code to Kaggle's Kernel
+as a single file, since the competition does not allow custom module beyond
+what is in the Kernel editor. 
+"""
+
 import random
 import os
 import regex
 
 
 PATH = './notebooks/library/'
-DOTTED_LINE =      '# ---------------------------------------------------------------\n' 
+DOTTED_LINE = '# ---------------------------------------------------------------\n' 
 SEPARATOR = '\n\n' +\
-            '# ----------------- SECTION AUTO ASSEMBLED ----------------------\n' + DOTTED_LINE
+              '# ----------------- SECTION AUTO ASSEMBLED ----------------------\n' + DOTTED_LINE
 
 def build(output_module_name = None, local_model_name = None):
+    """
+    Compiles a single file containing all source codes.
+
+    Parameters
+    --------------
+
+    output_module_name: (optional) the name of the file being outputed
+    local_model_name: (required) the name of the model orchestrator file
+    """
 
     def read_file(file):
         f = open(file, "r")
@@ -26,9 +42,6 @@ def build(output_module_name = None, local_model_name = None):
             content += SEPARATOR + m
         return content
 
-
-
-    
     if output_module_name is None:
         output_module_name = 'modelbuild' + str(random.randint(1, 100000)) + '.py'
 
