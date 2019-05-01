769c073f329c013ad2b9fe4a8283e10650b888b4
Giuseppe Romagnuolo <Giuseppe.Romagnuolo@allenovery.com>
2019-05-01 01:28:51 +0100

    Refactor: made stopwords path an input parameter

--- notebooks/library/ObsceneTextPreprocessor.py
+++ notebooks/library/ObsceneTextPreprocessor.py
@@ -15,12 +15,12 @@ class ObsceneTextPreprocessor():
     tokenizer = RegexpTokenizer(r'[\%\@\&\$\#\w\!\*]+')
 
 
-    def __init__(self, lower=False):
+    def __init__(self, lower=False, stop_words_path = './input/stopwords/english'):
         super().__init__()
 
         self.lower = lower
         #Use the English stopword list
-        self.__load_stopwords()
+        self.__load_stopwords(stop_words_path)
         # stop_words = set(stopwords.words('english'))
         self.stop_words = [w for w in self.stop_words if (not w in self.leave_words)]
 
@@ -30,15 +30,16 @@ class ObsceneTextPreprocessor():
             return w.lower()
         return w
 
-    def __load_stopwords(self):
-        f = open('../input/stopwords/english', 'r')
+    def __load_stopwords(self, stop_words_path):
+        # TODO: FIX for both local and Kaggle environment
+        f = open(stop_words_path, 'r')
         self.stop_words = f.readlines()
         f.close()
 
+    def clean_doc(self, doc):
+        out = [self.__lower_fn(w) for w in self.tokenizer.tokenize(doc) if (not w.lower() in self.stop_words)]
+        return out
+
     def clean(self, data):
 
-        def p(doc):
-            out = [self.__lower_fn(w) for w in self.tokenizer.tokenize(doc) if (not w.lower() in self.stop_words)]
-            return out
-
-        return [' '.join(p(doc)) for doc in data]
+        return [' '.join(self.clean_doc(doc)) for doc in data]
